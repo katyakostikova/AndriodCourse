7dc2488f0209424ded19896e8351598d2637efd7
Katya Kostikova <you@example.com>
2021-01-09 17:34:50 +0200

    les2

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("cat_jora", "qwert");
+        Button b = findViewById(R.id.button);
+        b.setText("My text");
+
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Button) v).setText("New text");
+            }
+        };
+
+        b.setOnClickListener(onClickListener);
     }
 }
