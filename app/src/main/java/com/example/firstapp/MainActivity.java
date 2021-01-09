c6dfc67537901e3c674bb2fbe667aaa8125cb4de
katyakostikova <katyakostikova21@gmail.com>
2021-01-10 01:20:14 +0200

    UI done

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.e("cat_jora", "qwert");
-        Button b = findViewById(R.id.button);
-        b.setText("My text");
-
-        View.OnClickListener onClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((Button) v).setText("New text");
-            }
-        };
-
-        b.setOnClickListener(onClickListener);
+        TextView numberOfContacts = findViewById(R.id.numberOfContacts);
+        numberOfContacts.setText("Number of contacts: " + 0);
     }
 }
