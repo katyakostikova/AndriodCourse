e40c0a324ac6d906087c4d0b07e5d6a801fadbb6
katyakostikova <katyakostikova21@gmail.com>
2021-01-10 13:11:50 +0200

    les 2 ht done

---
+++ app/src/main/java/com/example/firstapp/Contact.java
@@ -0,0 +1,14 @@
+package com.example.firstapp;
+
+public class Contact {
+    public String contactName;
+    public String phoneNumber;
+
+    public Contact(String name, String phoneNumber) {
+        this.contactName = name;
+        this.phoneNumber= phoneNumber;
+    }
+ }
+
+
+
--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,17 +1,88 @@
 package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
         TextView numberOfContacts = findViewById(R.id.numberOfContacts);
-        numberOfContacts.setText("Number of contacts: " + 0);
+        numberOfContacts.setText("Number of contacts: " + contacts.size());
+
+        Button addButton = findViewById(R.id.buttonAdd);
+
+        View.OnClickListener onAddButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.errorName).setVisibility(View.INVISIBLE);
+                findViewById(R.id.errorNumber).setVisibility(View.INVISIBLE);
+                EditText nameInput = (EditText)findViewById(R.id.nameInput);
+                String name = nameInput.getText().toString();
+                if (name.isEmpty()) {
+                    findViewById(R.id.errorName).setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                EditText numberInput = (EditText)findViewById(R.id.phoneInput);
+                String number = numberInput.getText().toString();
+                String regex = "(\\+\\d{12})";
+                Pattern numberCheck = Pattern.compile(regex);
+                Matcher numberRegexCheck = numberCheck.matcher(number.trim());
+                if (!numberRegexCheck.matches()) {
+                    Log.e("matcher", Boolean.toString(numberRegexCheck.matches()));
+                    findViewById(R.id.errorNumber).setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                contacts.add(new Contact(name.trim(), number.trim()));
+                numberOfContacts.setText("Number of contacts: " + contacts.size());
+            }
+        };
+
+        Button findButton = findViewById(R.id.buttonFind);
+
+        View.OnClickListener onFindButtonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.errorName).setVisibility(View.INVISIBLE);
+                findViewById(R.id.errorNumber).setVisibility(View.INVISIBLE);
+                EditText nameInput = (EditText)findViewById(R.id.nameInput);
+                String name = nameInput.getText().toString();
+                if (name.isEmpty()) {
+                    findViewById(R.id.errorName).setVisibility(View.VISIBLE);
+                    return;
+                }
+
+                EditText numberInput = (EditText)findViewById(R.id.phoneInput);
+                for (int i = 0; i < contacts.size(); i++) {
+                    if (contacts.get(i).contactName.equals(name)) {
+                        numberInput.setText(contacts.get(i).phoneNumber);
+                        return;
+                    }
+                }
+                nameInput.setText("Not found");
+                numberInput.setText("");
+            }
+        };
+
+        addButton.setOnClickListener(onAddButtonClickListener);
+        findButton.setOnClickListener(onFindButtonClickListener);
     }
 }
